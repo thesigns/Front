75b885f238ae5260bc615a9f83011b4cf7de7e41
Jakub <92330684+thesigns@users.noreply.github.com>
2022-11-07 19:50:11 +0100

    Dodaj pliki projektów.

---
+++ ProvingGround/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Front;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace ProvingGround {
+    internal class Program {
+
+        private static void Main(string[] args) {
+
+            VideoMode mode = new VideoMode(VideoMode.DesktopMode.Width, VideoMode.DesktopMode.Height);
+            ContextSettings contextSettings = new ContextSettings();
+            contextSettings.AntialiasingLevel = 8;
+            RenderWindow window = new RenderWindow(mode, "Front's Proving Ground", Styles.Fullscreen, contextSettings);
+            window.SetVerticalSyncEnabled(true);
+
+            window.Closed += (sender, args) => window.Close();
+
+            while (window.IsOpen) {
+                window.DispatchEvents();
+                window.Clear(Color.Black);
+
+                window.Display();
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape)) {
+                    window.Close();
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
